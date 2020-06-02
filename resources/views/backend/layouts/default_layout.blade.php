e973ba7e78dcfe2fb9e92b1708d8cdd3aff37e2d
JuJuRanger <60379977+JuJuRanger@users.noreply.github.com>
2020-06-02 11:46:01 +0700

    v3 commit

--- resources/views/backend/layouts/default_layout.blade.php
+++ resources/views/backend/layouts/default_layout.blade.php
@@ -8,7 +8,7 @@
     <!-- /.head Style -->
 </head>
 
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
     <!-- Site wrapper -->
     <div class="wrapper">
 
